293132aacba165a20df712fd56f61d98fa2039a5
Sibusiso <sibusisotntuli@gmail.com>
2023-02-22 13:19:18 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/*
+ *  main - print alphabet
+ *  print_alphabet - Make the alphabet
+ *  Return: void
+ */
+void print_alphabet(void)
+{
+char c;
+for(c = 'a'; c <= 'z'; c++)
+{
+	_putchar(c);
+}
+_putchar('\n');
+}
